93ebab7cee20a720623d7d5475afc9e44069e11f
Havilash Sivaratnam <havilash2005@gmail.com>
2024-12-29 18:08:39 +0100

    enable relative number in nvim

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -26,7 +26,7 @@ vim.g.icons = vim.g.have_nerd_font and {}
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
