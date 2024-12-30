128b3624a5e23c7e5bccb1063974e76daf07662a
Havilash Sivaratnam <havilash2005@gmail.com>
2024-12-30 18:13:52 +0100

    add some useful keymaps and fix command timeout

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -8,8 +8,18 @@ vim.g.maplocalleader = ' '
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Diagnostic keymaps
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- Better indent handling
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Move text up and down
+vim.keymap.set('v', 'J', ':m .+1<CR>==')
+vim.keymap.set('v', 'K', ':m .-2<CR>==')
+vim.keymap.set('x', 'J', ":move '>+1<CR>gv-gv")
+vim.keymap.set('x', 'K', ":move '<-2<CR>gv-gv")
+
+-- paste preserves primal yanked piece
+vim.keymap.set('v', 'p', '"_dP')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -19,7 +29,11 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- Non VSCode keymaps
 if not vim.g.vscode then
+  -- Diagnostic keymaps
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
   -- TIP: Disable arrow keys in normal mode
   vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!"<CR>')
   vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!"<CR>')
@@ -35,12 +49,14 @@ if not vim.g.vscode then
   vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
   vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+  -- Resize windows
   vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase window height' })
   vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease window height' })
   vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase window width' })
   vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease window width' })
-end
 
-vim.keymap.set('n', '<leader>?', function()
-  require('which-key').show { global = false }
-end, { desc = 'Buffer Local Keymaps (which-key)' })
+  -- See available commands using which key
+  vim.keymap.set('n', '<leader>?', function()
+    require('which-key').show { global = false }
+  end, { desc = 'Buffer Local Keymaps (which-key)' })
+end
