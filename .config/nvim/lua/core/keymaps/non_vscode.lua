d5eb16ee45b72201278ced0068e6c5369b458ab1
Havilash Sivaratnam <havilash2005@gmail.com>
2025-07-02 22:09:11 +0200

    feat(neovim): oil config

--- .config/nvim/lua/core/keymaps/non_vscode.lua
+++ .config/nvim/lua/core/keymaps/non_vscode.lua
@@ -24,6 +24,6 @@ vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increa
 vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease window width' })
 
 -- Workspace
-vim.keymap.set('n', '<leader>c', function()
+vim.keymap.set('n', '<leader>`', function()
   vim.cmd('cd ' .. vim.fn.expand '%:p:h')
-end, { desc = '[C]wd to buffer path' })
+end, { desc = 'Cwd to buffer path' })
