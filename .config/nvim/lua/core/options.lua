55ae8b49f6d271d950780d68e23a27543e64d45e
Havilash Sivaratnam <havilash.sivaratnam@gmail.com>
2025-08-01 21:11:28 +0200

    feat(neovim): change color scheme, make bg transparent

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -78,7 +78,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+-- vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
@@ -92,3 +92,15 @@ vim.opt.smarttab = true
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 vim.opt.confirm = true
+
+vim.cmd [[
+  hi NonText ctermbg=none guibg=NONE
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalNC guibg=NONE ctermbg=NONE
+  hi SignColumn ctermbg=NONE ctermfg=NONE guibg=NONE
+
+  hi Pmenu ctermbg=NONE ctermfg=NONE guibg=NONE
+  hi FloatBorder ctermbg=NONE ctermfg=NONE guibg=NONE
+  hi NormalFloat ctermbg=NONE ctermfg=NONE guibg=NONE
+  hi TabLine ctermbg=None ctermfg=None guibg=None
+]]
