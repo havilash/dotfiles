71ba0c861c7571916663c942c2da56e7bcaf0f7a
Havilash Sivaratnam <havilash.sivaratnam@gmail.com>
2025-08-01 20:14:31 +0200

    feat!: remove all old arch hyprland configs to for v2

--- .config/hypr/scripts/clipboard_history.sh
+++
@@ -1,3 +0,0 @@
-rm $HOME/.cache/cliphist/db # Wipe clipboard history FULLY
-wl-paste --type text --watch cliphist store & # Stores only text data
-wl-paste --type image --watch cliphist store & # Stores only image data
