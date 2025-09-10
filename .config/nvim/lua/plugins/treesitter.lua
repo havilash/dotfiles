5daa2f456d952bba7f66b27f16d744d197b5e631
Havilash Sivaratnam <havilash.sivaratnam@gmail.com>
2025-09-10 20:46:50 +0200

    feat: improve treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,10 @@ return { -- Highlight, edit, and navigate code
   enabled = not vim.g.vscode,
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    'nvim-treesitter/playground',
+  },
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = {
@@ -10,18 +14,28 @@ return { -- Highlight, edit, and navigate code
       'c',
       'diff',
       'html',
-      'lua',
-      'luadoc',
-      'markdown',
-      'markdown_inline',
-      'query',
-      'vim',
-      'vimdoc',
-      'html',
       'css',
       'scss',
-      'typescript',
       'javascript',
+      'jsdoc',
+      'json',
+      'jsonc',
+      'lua',
+      'luadoc',
+      'luap',
+      'markdown',
+      'markdown_inline',
+      'printf',
+      'python',
+      'query',
+      'regex',
+      'toml',
+      'tsx',
+      'typescript',
+      'vim',
+      'vimdoc',
+      'xml',
+      'yaml',
     },
     -- Autoinstall languages that are not installed
     auto_install = true,
@@ -33,6 +47,33 @@ return { -- Highlight, edit, and navigate code
       additional_vim_regex_highlighting = { 'ruby' },
     },
     indent = { enable = true, disable = { 'ruby' } },
+
+    -- incremental selection
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>', -- start selection
+        node_incremental = '<CR>', -- expand to bigger node
+        scope_incremental = '<S-CR>', -- expand to scope
+        node_decremental = '<BS>', -- shrink node
+      },
+    },
+
+    -- textobjects — requires the nvim-treesitter-textobjects dependency
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- jump forward to textobject
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+        },
+      },
+    },
   },
   -- There are additional nvim-treesitter modules that you can use to interact
   -- with nvim-treesitter. You should go explore a few and see what interests you:
